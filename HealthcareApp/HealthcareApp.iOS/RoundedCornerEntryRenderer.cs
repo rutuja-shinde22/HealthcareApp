0c4c5743dae732770533f46e918d63bb0db3c109
Rutuja Shinde <rutujashinde2013@gmail.com>
2020-08-28 15:43:31 +0530

    IOS Loaded and Build Success

---
+++ HealthcareApp/HealthcareApp.iOS/RoundedCornerEntryRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using HealthcareApp.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(RoundedCornerEntryRenderer))]
+namespace HealthcareApp.iOS
+{
+    public class RoundedCornerEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+
+                Control.BorderStyle = UITextBorderStyle.None;
+                Control.Layer.CornerRadius = 10;
+                Control.TextColor = UIColor.White;
+
+            }
+        }
+    }
+}
